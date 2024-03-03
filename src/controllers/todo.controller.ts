85f1b926eccf56c83d91a9f88f1d4cc666a7f130
Henrique de Castro <hendecastro@gmail.com>
2024-03-03 00:55:34 -0300

    feat(controllers): add create user controller

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from 'express'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function createTodoController(
+  request: Request,
+  response: Response,
+) {
+  const requestBodyParams = z.object({
+    title: z.string(),
+    description: z.string().optional(),
+  })
+
+  const body = requestBodyParams.safeParse(request.body)
+
+  if (body.success === false) {
+    return response.status(400).json({ error: 'Missing Required Data.' })
+  }
+
+  const { title, description } = body.data
+
+  await prisma.todo.create({
+    data: {
+      title,
+      description,
+    },
+  })
+
+  return response.status(201).send()
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {
   findUserByIdController,
   listUserController,
 } from './controllers/user.controller'
+import { createTodoController } from './controllers/todo.controller'
 
 const app = express()
 const port = 3333
@@ -15,6 +16,8 @@ app.get('/users', listUserController)
 app.get('/users/:userId', findUserByIdController)
 
 app.post('/users', createUserController)
+
+app.post('/todo', createTodoController)
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
